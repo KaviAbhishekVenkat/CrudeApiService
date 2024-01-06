c453632d35541beb839c990530aefa6da5fb375f
kaviabhishekvenkat <kaviabhishekvenkat01@outlook.com>
2024-01-06 12:42:29 +0530

    Initial commit

---
+++ src/main/java/com/kav/CrudeApiService/CrudeApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kav.CrudeApiService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudeApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudeApiServiceApplication.class, args);
+	}
+
+}
